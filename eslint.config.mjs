8be33fede4e72a524402fd25a0fcf15e588d2c03
M-Asimm <kpkprince55persia@gmail.com>
2025-10-17 02:54:26 +0500

    eslint comfiguraion

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -19,7 +19,26 @@ const eslintConfig = [
       "build/**",
       "next-env.d.ts",
     ],
+    rules:{
+      "react/react-in-jsx-scope": "off",
+      "react/jsx-key": "off",
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
+      "no-console": "warn",
+      "no-unused-vars": ["warn", { "argsIgnorePattern": "^_" }],
+      "semi": ["error", "always"],
+      "quotes": ["error", "double"],
+      "jsx-quotes": ["error", "prefer-double"],
+      "comma-dangle": ["error", "always-multiline"],
+      "object-curly-spacing": ["error", "always"],
+      "array-bracket-spacing": ["error", "never"],
+      "arrow-parens": ["error", "always"],
+      "prefer-const": ["error", {
+        destructuring: "all"
+      }],
+    }
   },
+  
 ];
 
 export default eslintConfig;
